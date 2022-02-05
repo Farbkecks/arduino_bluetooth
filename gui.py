88cf4474cf05941237965a5c97dd65a7c13a7bf3
farbkecks <fabian.alich@gmx.de>
2022-02-05 09:41:46 +0100

    edit gui

--- gui.py
+++ gui.py
@@ -14,20 +14,20 @@ root.resizable(width=False, height=False)
 root.title("Steuerung für Arduino")
 
 label_verbindung = tk.Label(root, text="bluetooth Verbindung", bg="red")
-label_verbindung.pack(fill="both");
-label_verbindung.config(width=400, height=100)
+label_verbindung.pack(fill="both", expand=True);
+# label_verbindung.config(width=400, height=100)
 
 lable_w = tk.Label(root, text="Motor", bg="red")
 lable_w.pack(expand=True, fill="both")
-lable_w.config(width=400, height=100)
+# lable_w.config(width=400, height=100)
 
 lable_a = tk.Label(root, text="drehung links", bg="red")
 lable_a.pack(expand=True,side= "left", fill="both")
-lable_a.config(width=400, height=100)
+# lable_a.config(width=400, height=100)
 
 lable_d = tk.Label(root, text="drehung rechts", bg="red")
-lable_d.pack(expand=True,side = "right", fill="both")
-lable_d.config(width=400, height=100)
+lable_d.pack(expand=True,side ="right", fill="both")
+# lable_d.config(width=400, height=100)
 
 
 root.mainloop()
