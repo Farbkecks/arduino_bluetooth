69d62ddf6124a767576692f60d2d2f55db335e6d
farbkecks <fabian.alich@gmx.de>
2022-02-05 10:12:47 +0100

    cleaner code

--- basic_bluetooth.ino
+++ basic_bluetooth.ino
@@ -5,16 +5,15 @@ Servo servoblau;
 SoftwareSerial serial_connection(10, 11);//Create a serial connection with TX and RX on these pins
 
 char inData;
-int winkel = 90;
+int WINKEL = 90;
 int LED = 3;
 int SCHRITT =  10;
 
 void setup()
 {
-  Serial.begin(9600);//Initialize communications to the serial monitor in the Arduino IDE
+  // Serial.begin(9600);//Initialize communications to the serial monitor in the Arduino IDE
   serial_connection.begin(9600);//Initialize communications with the bluetooth module
-  serial_connection.println("Ready!!!");//Send something to just start comms. This will never be seen.
-  Serial.println("Started");//Tell the serial monitor that the sketch has started.
+  // Serial.println("Started");//Tell the serial monitor that the sketch has started.
   pinMode(LED, OUTPUT);
   servoblau.attach(8);
 }
@@ -23,21 +22,20 @@ void loop()
   byte byte_count=serial_connection.available();//This gets the number of bytes that were sent by the python script
   if(byte_count)//If there are any bytes then deal with them
   {
-
     inData=serial_connection.read();
-    Serial.println(inData);
+    // Serial.println(inData);
     if(inData=='0'){
       digitalWrite(LED,0);
     }
     if(inData=='1'){
-      if(winkel<180){
-        winkel = winkel + SCHRITT;
+      if(WINKEL<180){
+        WINKEL = WINKEL + SCHRITT;
       }
       digitalWrite(LED, 0);
     }
     if(inData=='2'){
-      if(winkel>0){
-        winkel = winkel - SCHRITT;
+      if(WINKEL>0){
+        WINKEL = WINKEL - SCHRITT;
       }
       digitalWrite(LED, 0);
     }
@@ -45,19 +43,18 @@ void loop()
       digitalWrite(LED, 1);
     }
     if(inData=='4'){
-      if(winkel<180){
-        winkel = winkel + SCHRITT;
+      if(WINKEL<180){
+        WINKEL = WINKEL + SCHRITT;
       }
       digitalWrite(LED, 1);
     }
     if(inData=='5'){
-      if(winkel>0){
-        winkel = winkel - SCHRITT;
+      if(WINKEL>0){
+        WINKEL = WINKEL - SCHRITT;
       }
       digitalWrite(LED, 1);
     }
-    servoblau.write(winkel);
-    
+    servoblau.write(WINKEL);
   }
   delay(100);//Pause for a moment 
 }
